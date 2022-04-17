cef49a8a9eb1c4286287f2b0fc33de2fb72ddcf9
Abdul <abdulmomin956@gmail.com>
2022-04-17 19:42:06 +0600

    login register route created

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import { Button, Form } from 'react-bootstrap';
 
 const Login = () => {
+    const handleSubmit = event => {
+        event.preventDefault();
+    }
     return (
         <div>
-            <h1>Please Login</h1>
+            <h1 className='text-center'>Please Login</h1>
+            <div className="w-50 mx-auto">
+                <Form onSubmit={handleSubmit}>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" />
+                        <Form.Text className="text-muted">
+                            We'll never share your email with anyone else.
+                        </Form.Text>
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password" />
+                    </Form.Group>
+                    <Button variant="dark" type="login">
+                        Submit
+                    </Button>
+                </Form>
+            </div>
         </div>
     );
 };
--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,10 +1,39 @@
 import React from 'react';
+import { Button, Form } from 'react-bootstrap';
 
 const Register = () => {
+    const handleSubmit = event => {
+        event.preventDefault();
+    }
     return (
+
         <div>
-            <h1>Register</h1>
+            <h1 className='text-center'>Please Register</h1>
+            <div className="w-50 mx-auto">
+                <Form onSubmit={handleSubmit}>
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Full Name</Form.Label>
+                        <Form.Control type="text" placeholder="Full Name" />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" />
+                        {/* <Form.Text className="text-muted">
+                            We'll never share your email with anyone else.
+                        </Form.Text> */}
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password" />
+                    </Form.Group>
+                    <Button variant="dark" type="register">
+                        Register
+                    </Button>
+                </Form>
+            </div>
         </div>
+
     );
 };
 
